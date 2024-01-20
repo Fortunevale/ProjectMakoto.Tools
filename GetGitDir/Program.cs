d2f03874f5897d115d6f2269de7f5b687a6488be
Mira <56395159+TheXorog@users.noreply.github.com>
2024-01-20 13:52:52 +0100

    nerushfgbhjkze4ghbieurzdh789izouehzbugk4789ziogh

---
+++ GetGitDir/Program.cs
@@ -0,0 +1,20 @@
+﻿try
+{
+    if (!new FileInfo(".git").Exists)
+        return 1;
+
+    var content = File.ReadAllLines(".git");
+
+    if (!content.Any(x => x.StartsWith("gitdir: ")))
+        return 1;
+
+    var gitDirLine = content.First(x => x.StartsWith("gitdir: "));
+
+    Console.WriteLine(gitDirLine[8..]);
+    return 0;
+
+}
+catch (Exception)
+{
+    return 1;
+}
---
+++ TranslationSourceGenerator/RunDefault.sh
@@ -0,0 +1,1 @@
+dotnet run -- ../../ProjectMakoto/Translations/strings.json ../../ProjectMakoto/Entities/Translation/Translations.cs ProjectMakoto.Entities
