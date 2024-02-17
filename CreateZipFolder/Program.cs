00a52543b8a082a3a48ab990b698b889d7c785fb
Mira <56395159+TheXorog@users.noreply.github.com>
2024-02-17 15:40:47 +0100

    feat: Add Zip Tool

---
+++ CreateZipFolder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO.Compression;
+
+var env = Environment.GetCommandLineArgs();
+
+if (env.Length < 3)
+{
+    Console.WriteLine("Arguments required: [Directory] [OutputFile]");
+    return 1;
+}
+
+if (!new DirectoryInfo(env[1]).Exists)
+{
+    Console.WriteLine($"'{env[1]}' does not exist.");
+    return 1;
+}
+
+if (new FileInfo(env[2]).Exists)
+{
+    Console.WriteLine($"'{env[2]}' already exists.");
+    return 1;
+}
+
+ZipFile.CreateFromDirectory(env[1], env[2]);
+return 0;
